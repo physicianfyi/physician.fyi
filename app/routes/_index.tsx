cc47a4f20b3aee9986ef55a9bbae508824a681be
Physician Fyi <physicianfyi@gmail.com>
2023-10-09 16:03:39 -0700

    clarify name search format

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -801,7 +801,7 @@ export default function Index() {
           <div className="relative w-full">
             <input
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="e.g., John or G 12345"
+              placeholder="e.g., 'Smith, John' or 'G 12345'"
               name="q"
               // TODO defaultValue doesn't get reset when clearing query, expected but weird
               defaultValue={query}
