fa52bd5ab54c5c03b71c0dadc5d196cc366a275f
Physician Fyi <physicianfyi@gmail.com>
2023-10-20 21:22:28 -0700

    add search to filterable select

--- app/components/FilterableMultiSelect.tsx
+++ app/components/FilterableMultiSelect.tsx
@@ -1,6 +1,7 @@
 import type { Dispatch, ReactNode, SetStateAction } from "react";
-import { useState } from "react";
+import { startTransition, useMemo, useState } from "react";
 import * as Ariakit from "@ariakit/react";
+import Fuse from "fuse.js";
 
 interface Props {
   label: ReactNode;
@@ -26,73 +27,109 @@ export const FilterableMultiSelect = ({
   nullFallback = "N/A",
 }: Props) => {
   const [mounted, setMounted] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
+
+  const matches = useMemo(() => {
+    if (!searchValue) {
+      // Don't do with items without copying since it's in place
+      return Object.keys(counts)
+        .sort((a, b) => counts[b] - counts[a])
+        .map((key) => ({ item: key }));
+    }
+    const fuse = new Fuse(Object.keys(counts), {
+      includeScore: false,
+    });
+    return fuse.search(searchValue);
+  }, [counts, searchValue]);
+
   return (
     <div className="flex flex-col gap-1">
-      {/* @ts-ignore ariakit TS issue */}
-      <Ariakit.SelectProvider
-        // Don't think needed
-        // defaultValue={values}
-        value={values}
-        setValue={setValues}
-        setMounted={setMounted}
-        focusLoop
+      <Ariakit.ComboboxProvider
+        // TODO Reset value on select
+        resetValueOnHide
+        setValue={(value) => {
+          startTransition(() => {
+            setSearchValue(value);
+          });
+        }}
       >
-        <div className="flex items-end justify-between">
-          <Ariakit.SelectLabel className="select-label">
-            {label}
-          </Ariakit.SelectLabel>
-          {values.length > 0 && (
-            <button
-              type="button"
-              className="text-xs hover:underline"
-              onClick={() => {
-                setValues([]);
-              }}
-            >
-              Clear
-            </button>
-          )}
-        </div>
-        <Ariakit.Select className="flex justify-between items-center py-2.5 px-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
-          <div className="flex items-center gap-2 flex-wrap">
-            {values.map((v) => (
-              <div
-                key={v}
-                className="flex items-center gap-1 bg-blue-100 whitespace-nowrap text-blue-800 text-xs font-medium px-2 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300"
+        {/* @ts-ignore ariakit TS issue */}
+        <Ariakit.SelectProvider
+          // Don't think needed
+          // defaultValue={values}
+          value={values}
+          setValue={setValues}
+          setMounted={setMounted}
+          focusLoop
+        >
+          <div className="flex items-end justify-between">
+            <Ariakit.SelectLabel className="select-label">
+              {label}
+            </Ariakit.SelectLabel>
+            {values.length > 0 && (
+              <button
+                type="button"
+                className="text-xs hover:underline"
+                onClick={() => {
+                  setValues([]);
+                }}
               >
-                <div>{items[v] ?? nullFallback}</div>
-                <div className="bg-white rounded-full px-1">
-                  {counts[items[v] ?? "null"]}
-                </div>
-              </div>
-            ))}
+                Clear
+              </button>
+            )}
           </div>
-          <Ariakit.SelectArrow />
-        </Ariakit.Select>
-        {mounted && (
-          <Ariakit.SelectPopover gutter={4} sameWidth className="popover">
-            {/* Don't do with items without copying since it's in place */}
-            {Object.keys(counts)
-              .sort((a, b) => counts[b] - counts[a])
-              .map((key: string) => (
-                <Ariakit.SelectItem
-                  key={`action-${key}`}
-                  // @ts-ignore TODO File ticket with ariakit to allow number
-                  value={items.indexOf(key)}
-                  className="select-item"
+          <Ariakit.Select className="flex justify-between items-center py-2.5 px-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+            <div className="flex items-center gap-2 flex-wrap">
+              {values.map((v) => (
+                <div
+                  key={v}
+                  className="flex items-center gap-1 bg-blue-100 whitespace-nowrap text-blue-800 text-xs font-medium px-2 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300"
                 >
-                  <Ariakit.SelectItemCheck />
-                  <div className="[&>*]:align-middle">
-                    <span>{key === "null" ? nullFallback : key} </span>
-                    <span className="bg-white rounded-full px-1 text-black text-xs">
-                      {counts[key]}
-                    </span>
+                  <div>{items[v] ?? nullFallback}</div>
+                  <div className="bg-white rounded-full px-1">
+                    {counts[items[v] ?? "null"]}
                   </div>
-                </Ariakit.SelectItem>
+                </div>
               ))}
-          </Ariakit.SelectPopover>
-        )}
-      </Ariakit.SelectProvider>
+            </div>
+            <Ariakit.SelectArrow />
+          </Ariakit.Select>
+          {mounted && (
+            <Ariakit.SelectPopover
+              gutter={4}
+              sameWidth
+              className="popover pt-0"
+            >
+              <div className="sticky top-0 mb-2 w-full bg-inherit pt-2">
+                <Ariakit.Combobox
+                  autoSelect
+                  placeholder="Search..."
+                  className="w-full h-10 rounded border-none px-4 leading-4"
+                />
+              </div>
+              <Ariakit.ComboboxList>
+                {matches.map(({ item: key }) => (
+                  <Ariakit.SelectItem
+                    key={`action-${key}`}
+                    // @ts-ignore TODO File ticket with ariakit to allow number
+                    value={items.indexOf(key)}
+                    className="scroll-m-2 scroll-mt-14 p-1 flex items-center gap-2 cursor-pointer data-[active-item]:bg-accent rounded-sm aria-selected:bg-accent/50"
+                    render={<Ariakit.ComboboxItem />}
+                  >
+                    <Ariakit.SelectItemCheck />
+                    <div className="[&>*]:align-middle">
+                      <span>{key === "null" ? nullFallback : key} </span>
+                      <span className="bg-white rounded-full px-1 text-black text-xs">
+                        {counts[key]}
+                      </span>
+                    </div>
+                  </Ariakit.SelectItem>
+                ))}
+              </Ariakit.ComboboxList>
+            </Ariakit.SelectPopover>
+          )}
+        </Ariakit.SelectProvider>
+      </Ariakit.ComboboxProvider>
     </div>
   );
 };
