89a0fd2307158ac5dbd1a593169a7c8fc2c9d3e0
Physician Fyi <physicianfyi@gmail.com>
2023-10-21 01:54:31 -0700

    archive fl

--- generate/fl/archive.ts
+++ generate/fl/archive.ts
@@ -1,5 +1,5 @@
 /**
- * Archive pdfs from MBC
+ * Archive pdfs from MBF
  */
 
 import fs from "fs";
@@ -8,17 +8,22 @@ import { delay } from "../util";
 // TODO Archive the license pages too and mark for rearchival when they change
 
 (async () => {
-  const data = JSON.parse(fs.readFileSync("data/ca/scrape-deep.json", "utf8"));
+  const data = JSON.parse(fs.readFileSync("data/fl/scrape-deep.json", "utf8"));
+
   let archive: any = { pdfs: {} };
   try {
-    archive = JSON.parse(fs.readFileSync("data/ca/archive.json", "utf8"));
+    archive = JSON.parse(fs.readFileSync("data/fl/archive.json", "utf8"));
   } catch {}
 
   const errors: any = {};
 
   for (let [, v] of Object.entries<any>(data.profiles)) {
     for (let action of v.actions ?? []) {
-      const url = action.url;
+      const documentId = action.documentId;
+      const url = documentId
+        ? `https://mqa-internet.doh.state.fl.us/MQASearchServices/Document?id=${documentId}`
+        : null;
+
       if (!url || archive.pdfs.hasOwnProperty(url)) continue;
 
       console.log(url);
@@ -35,12 +40,14 @@ import { delay } from "../util";
         archive.pdfs[url] = new Date();
         archive.lastRun = new Date();
         fs.writeFile(
-          "data/ca/archive.json",
+          "data/fl/archive.json",
           JSON.stringify(archive),
           (error) => {
             if (error) throw error;
           }
         );
+      } else {
+        console.error(response.status);
       }
 
       // Without proxies need to wait 20 seconds to avoid 429s
